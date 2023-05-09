faa7bf54e9752ca9c8339ff8e5f1e8f0ebfe14eb
CHOI INSEONG <cINSEONG0625@gmail.com>
2023-05-10 01:22:23 +0900

    Job

--- Job.py
+++ Job.py
@@ -3,36 +3,31 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from fake_useragent import  UserAgent
 import time
+class Crawler:
+    def __init__(self):
+        options = Options()
+        self.url = "https://www.saramin.co.kr/zf_user/"
+        userAgent = UserAgent()
+        options.add_argument(f"user-agent = {userAgent.random}")
+        self.driver = webdriver.Chrome()
 
-def OpenSite():
-    options = Options()
-    url = "https://www.saramin.co.kr/zf_user/"
-    userAgent = UserAgent()
-    options.add_argument(f"user-agent = {userAgent.random}")
-    driver = webdriver.Chrome()
-    driver.get(url)
-    time.sleep(5)
-    job_input = driver.find_element(By.XPATH, "//*[@id=\"btn_search\"]")
-    job_input.click()
-    time.sleep(2)
-    keyword_Sender = driver.find_element(By.XPATH, "//*[@id=\"ipt_keyword_recruit\"]")
-    keyword_Sender.send_keys('개발자')
-    time.sleep(2)
-    keyword_search = driver.find_element(By.XPATH, "//*[@id=\"btn_search_recruit\"]")
-    keyword_search.click()
-    time.sleep(5)
-    driver.find_element(By.XPATH, "//*[@id=\"content\"]/ul[1]/li[2]/a").click()
-    time.sleep(5)
-    return driver
-
-
-def SearchJob(keyword):
-    driver = OpenSite()
-    driver.find_element(By.XPATH, "//*[@id=\"btn_search\"]").click()
-
-
+    def SearchJob(self, keyword):
+        self.driver.get(self.url)
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, "//*[@id=\"btn_search\"]").click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//*[@id=\"ipt_keyword_recruit\"]").send_keys(keyword)
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//*[@id=\"btn_search_recruit\"]").click()
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, "//*[@id=\"content\"]/ul[1]/li[2]/a").click()
+        time.sleep(5)
+        job_list = self.driver.find_elements(By.CLASS_NAME, "job_tit")
+        for job in job_list:
+            print(job.text)
 
 
 
 if __name__ == '__main__':
-    OpenSite()
+    job_list = Crawler()
+    job_list.SearchJob("호텔리어")
